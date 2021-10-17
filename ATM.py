46f5df817ab5dda4f778d55b0f51bd1025209c9e
The-Tank-Shivanshu <91969064+The-Tank-Shivanshu@users.noreply.github.com>
2021-10-17 13:12:09 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,14 @@
+class ATM(object):
+    def __init__(self,name, paiseOthao, prank):
+        self.name=name
+        self.paiseOthao=paiseOthao
+        self.prank=prank
+    
+    def paiseOthao(cash):
+        print(cash,"WithDrawed")
+    
+    def prank():
+        print("you are looted:) have a nice day")
+louis=ATM("Louis XVI",300,600)
+louis.paiseOthao(200)
+louis.balanceEnquiry()
